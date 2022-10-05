bc164df2bc9278944d536b7827cddf1ed8f2f875
barankibar <56038362+barankibar@users.noreply.github.com>
2022-10-06 00:46:44 +0300

    Add dynamic posts & AddNewPost Page

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const ejs = require("ejs");
+
+const path = require("path");
+
+const { execArgv } = require("process");
+
 const express = require("express");
 
 const app = express();
 
+const {default: mongoose} = require("mongoose");
+
+const Post = require(__dirname + "/models/Posts.js");
+
+// Connect DB
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 // MIDDLEWARE
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.render("index");
+// ROUTES
+app.get("/", async (req, res) => {
+    const posts = await Post.find({});
+    res.render("index", {
+        posts
+    });
+    console.log(posts);
 });
 
 app.get("/about", (req, res) => {
@@ -20,6 +41,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
     res.render("add_post");
+});
+
+app.post("/AddNewPost", async (req, res) => {
+    await Post.create(req.body);
+    res.redirect("/");
 });
 
 const port = 3000;
